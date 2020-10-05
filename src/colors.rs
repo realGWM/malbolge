b4b677013aca8e2517fc052d1907ec125907522f
Nazar Kalinowski <nazark@tutanota.com>
2020-10-05 18:40:37 +0300

    Add blue

--- src/colors.rs
+++ src/colors.rs
@@ -3,6 +3,7 @@ pub trait Colors {
     fn true_color(&self, r: u8, g: u8, b: u8) -> String;
     fn red(&self) -> String;
     fn green(&self) -> String;
+    fn blue(&self) -> String;
 }
 
 impl Colors for str {
@@ -17,5 +18,9 @@ impl Colors for str {
 
     fn green(&self) -> String {
         format!("\x1b[32m{}\x1b[0m", &self)
+    }
+
+    fn blue(&self) -> String {
+        format!("\x1b[34m{}\x1b[0m", &self)
     }
 }
