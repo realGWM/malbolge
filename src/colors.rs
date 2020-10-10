cb4334501bb5792a29370afe1234b9dbac8aaf34
Nazar Kalinowski <nazark@tutanota.com>
2020-10-10 21:22:40 +0300

    Remove double-reference

--- src/colors.rs
+++ src/colors.rs
@@ -9,18 +9,18 @@ pub trait Colors {
 impl Colors for str {
 
     fn true_color(&self, r: u8, g: u8, b: u8) -> String {
-        format!("\x1b[38;2;{};{};{}m{}\x1b[0m", r, g, b, &self)
+        format!("\x1b[38;2;{};{};{}m{}\x1b[0m", r, g, b, self)
     }
 
     fn red(&self) -> String {
-        format!("\x1b[31m{}\x1b[0m", &self)
+        format!("\x1b[31m{}\x1b[0m", self)
     }
 
     fn green(&self) -> String {
-        format!("\x1b[32m{}\x1b[0m", &self)
+        format!("\x1b[32m{}\x1b[0m", self)
     }
 
     fn blue(&self) -> String {
-        format!("\x1b[34m{}\x1b[0m", &self)
+        format!("\x1b[34m{}\x1b[0m", self)
     }
 }
